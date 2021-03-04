c1c8e206af581c40941b420526d573238318fd26
Brian Hackett <bhackett1024@gmail.com>
2021-03-04 09:12:02 -0700

    Support using container to run tests

--- run.ts
+++ run.ts
@@ -1,4 +1,4 @@
-import fs from "fs";
+import * as fs from "fs";
 import { spawnSync } from "child_process";
 
 const Usage = `
@@ -6,23 +6,31 @@ Usage: ts-node run.ts options
 Options:
   --update             Force update to playwright browser.
   --recordings <path>  File to store recording IDs.
+  --container          Use docker container for running tests (default on linux).
+  --env <key> <value>  Set environment variable in tests.
   <test>               Run given test once.
-Browsers are installed at $REPLAY_PLAYWRIGHT_BROWSER, defaulting to $HOME/replay-playwright-browser
+
+On macOS, browsers are installed at $REPLAY_PLAYWRIGHT_BROWSER, defaulting to $HOME/.replay-playwright-browser
 `;
 
 let gNeedUpdate = false;
 let gRecordingFile: string | undefined;
+let gUseContainer = process.platform == "linux";
 const gTests: string[] = [];
+const gEnvironment: Record<string, string> = {};
 for (let i = 2; i < process.argv.length; i++) {
   const arg = process.argv[i];
   if (arg == "--update") {
     gNeedUpdate = true;
   } else if (arg == "--recordings") {
     gRecordingFile = process.argv[++i];
+  } else if (arg == "--container") {
+    gUseContainer = true;
+  } else if (arg == "--env") {
+    gEnvironment[process.argv[++i]] = process.argv[++i];
   } else {
-    const test = `${__dirname}/${arg}`;
-    if (fs.existsSync(test)) {
-      gTests.push(test);
+    if (fs.existsSync(`${__dirname}/${arg}`)) {
+      gTests.push(arg);
     } else {
       console.log(`Unknown test ${arg}`);
       console.log(Usage);
@@ -42,7 +50,7 @@ if (gTests.length && !gRecordingFile) {
   process.exit(1);
 }
 
-const BrowserDir = process.env.REPLAY_PLAYWRIGHT_BROWSER || `${process.env.HOME}/replay-playwright-browser`;
+const BrowserDir = process.env.REPLAY_PLAYWRIGHT_BROWSER || `${process.env.HOME}/.replay-playwright-browser`;
 
 // This will need updating when we move to a more recent version of playwright.
 const BrowserSubdir = `${BrowserDir}/firefox-1225`;
@@ -54,10 +62,24 @@ function mkdirSyncIfNotExists(dir: string) {
 }
 
 function updateBrowser() {
+  if (gUseContainer) {
+    if (!gNeedUpdate) {
+      const { stdout } = spawnSync("docker", ["image", "ls", "recordreplayinc/playwright:latest"]);
+      if (stdout.includes("recordreplayinc/playwright")) {
+        return;
+      }
+    }
+
+    spawnSync("docker", ["image", "pull", "recordreplayinc/playwright:latest"], { stdio: "inherit" });
+    return;
+   }
+
   if (process.platform == "darwin") {
     if (!gNeedUpdate && fs.existsSync(`${BrowserSubdir}/firefox`)) {
       return;
     }
+
+    spawnSync("rm", ["-rf", BrowserSubdir]);
 
     mkdirSyncIfNotExists(BrowserDir);
     mkdirSyncIfNotExists(BrowserSubdir);
@@ -70,18 +92,63 @@ function updateBrowser() {
       stdio: "inherit",
     });
     spawnSync("tar", ["xf", "macOS-replay-playwright.tar.xz"], { cwd: BrowserSubdir });
+    return;
   }
 }
 
 async function runTest(test: string) {
+  const server = process.env.RECORD_REPLAY_SERVER || "wss://dispatch.replay.io";
+
+  if (gUseContainer) {
+    const tmpRecordingFile = `recordings-${(Math.random() * 1e9) | 0}.log`;
+    const tmpRecordingPath = `${__dirname}/${tmpRecordingFile}`;
+    if (fs.existsSync(gRecordingFile)) {
+      fs.copyFileSync(gRecordingFile, tmpRecordingPath);
+    }
+
+    const script = `
+npm i -g typescript ts-node playwright@1.8.1
+npm i --save-dev @types/node
+ts-node playwright-tests/${test}
+`;
+    const tmpScriptFile = `run-${(Math.random() * 1e9) | 0}`;
+    const tmpScriptPath = `${__dirname}/${tmpScriptFile}`;
+    fs.writeFileSync(tmpScriptPath, script);
+
+    const envArgs = [];
+    for (const [key, value] of Object.entries(gEnvironment)) {
+      envArgs.push("-e", `${key}=${value}`);
+    }
+
+    spawnSync("docker", [
+      "run",
+      "-v", `${__dirname}:/playwright-tests`,
+      "-e", `RECORD_REPLAY_RECORDING_ID_FILE=/playwright-tests/${tmpRecordingFile}`,
+      "-e", `RECORD_REPLAY_SERVER=${server}`,
+      "-e", "RECORD_ALL_CONTENT=1",
+      ...envArgs,
+      "recordreplayinc/playwright:latest",
+      "bash",
+      `/playwright-tests/${tmpScriptFile}`,
+    ], { stdio: "inherit" });
+
+    if (fs.existsSync(tmpRecordingPath)) {
+      fs.copyFileSync(tmpRecordingPath, gRecordingFile);
+      fs.unlinkSync(tmpRecordingPath);
+    }
+
+    fs.unlinkSync(tmpScriptPath);
+    return;
+  }
+
   if (process.platform == "darwin") {
     const driver = process.env.RECORD_REPLAY_DRIVER || `${BrowserSubdir}/macOS-recordreplay.so`;
-    const server = process.env.RECORD_REPLAY_SERVER || "wss://dispatch.replay.io";
 
-    spawnSync("ts-node", [test], {
+    spawnSync("ts-node", [`${__dirname}/${test}`], {
       stdio: "inherit",
       env: {
         ...process.env,
+        ...gEnvironment,
         PLAYWRIGHT_BROWSERS_PATH: BrowserDir,
         RECORD_REPLAY_DRIVER: driver,
         RECORD_REPLAY_RECORDING_ID_FILE: gRecordingFile,
@@ -89,6 +156,7 @@ async function runTest(test: string) {
         RECORD_ALL_CONTENT: "1",
       },
     });
+    return;
   }
 }
 
