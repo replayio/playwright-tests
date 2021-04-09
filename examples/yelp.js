a66d398db3de38d090701f66b9325b5f4fe18acf
Ryan Duffy <ryan.duffy@lge.com>
2021-04-09 15:39:53 -0700

    Refactor yelp example to adopt new helpers

--- examples/yelp.js
+++ examples/yelp.js
@@ -1,21 +1,34 @@
 const { example } = require("../src/helpers");
 
+// Searches for results on the main page or via header on detail page
+function search(text) {
+  return async (page) => {
+    const searchFormSelector = "form[action='/search']";
+    const inputSelector = `${searchFormSelector} input:not([tabIndex = "-1"])`;
+    const submitSelector = `${searchFormSelector} button[type='submit']`;
+
+    await page.click(inputSelector);
+    await page.fill(inputSelector, text);
+    await page.click(submitSelector);
+  };
+}
+
+// returns the nth (0-based) non-sponsored result
+function selectNthResult(n = 0) {
+  return async (page) => {
+    const elements = await page.$$('li h4 a[href *= "/biz/"]');
+    if (elements[n]) {
+      await elements[n].click();
+    }
+  };
+}
+
 example("Find a thai restaurant on yelp", async (page, { action }) => {
-  await action("Search for a thai restaurant", async () => {
-    await page.goto("https://www.yelp.com/");
+  await page.goto("https://www.yelp.com/");
 
-    // NOTE: (plumbers, delivery, takeout) is not locale friendly
-    await page.click('input[placeholder="plumbers, delivery, takeout..."]');
-    await page.fill("input[placeholder]", "thai");
-    await page.click("//span[normalize-space(.)='Search']/span[1]");
-  });
+  await action("Search for a thai restaurant", search("thai"));
+  await action("Click on the first result", selectNthResult());
 
-  await action("Click on the first result", () =>
-    Promise.all([
-      // waiting on url 'https://www.yelp.com/biz/thaibodia-bistro-campbell-campbell'
-      // NOTE: we need a more general selector
-      page.waitForNavigation(),
-      page.click("h4 a"),
-    ])
-  );
+  await action("Search for a taco restaurant", search("taco"));
+  await action("Click on the first result", selectNthResult());
 });
