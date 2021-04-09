d7284b4cabbee15867e4141b72a1e79da764f965
Ryan Duffy <ryan.duffy@lge.com>
2021-04-09 12:46:46 -0700

    Update helper utilities with in-browser logging

--- src/helpers.js
+++ src/helpers.js
@@ -5,30 +5,61 @@ const launchOptions = {
   headless: false,
 };
 
-async function example(name, cbk) {
-  try {
-    const browser = await playwright[browserName].launch(launchOptions);
-    const context = await browser.newContext();
-    const page = await context.newPage();
-
-    console.log(`Starting ${name}`);
-    await cbk(page);
-    console.log(`Finishing ${name}`);
-
-    await page.close();
-    await context.close();
-    await browser.close();
-  } catch (e) {
-    console.error(e);
+let depth = 0;
+const log = (...args) => {
+  if (depth) {
+    console.log("".padStart(depth * 2).substr(1), ...args);
+  } else {
+    console.log(...args);
   }
+};
+
+function wrapped(cbk, pageLog = log) {
+  return async (name, ...args) => {
+    try {
+      await pageLog(`> ${name}`);
+      depth++;
+      return await cbk(...args);
+    } catch (e) {
+      console.error(`Error in ${name}`);
+      console.error(e);
+    } finally {
+      depth--;
+      await pageLog(`< ${name}`);
+    }
+  };
 }
 
-async function action(name, cbk) {
-  console.log(name);
-  await cbk();
-}
+const action = wrapped(async (cbk) => await cbk());
+
+const example = wrapped(async (cbk) => {
+  const browser = await playwright[browserName].launch(launchOptions);
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  log("Browser launched");
+
+  // Create page-bound actions
+  const pageLog = (...args) => {
+    log(...args);
+    return page.evaluate((extArgs) => {
+      console.log("Test Step:", ...extArgs);
+    }, args);
+  };
+  const pageAction = wrapped(async (cbk) => await cbk(), pageLog);
+
+  await action(
+    "Running example",
+    async () => await cbk(page, { log: pageLog, action: pageAction })
+  );
+
+  await page.close();
+  await context.close();
+  await browser.close();
+});
 
 module.exports = {
-  example,
   action,
+  example,
+  log,
 };
