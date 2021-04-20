56c18dcf45c722dab09b1c55866c7f78e8efb37e
Ryan Duffy <ryan.duffy@lge.com>
2021-04-20 09:43:09 -0700

    Refactor whatismyuseragent to use example helper

--- examples/whatsmyuseragent.js
+++ examples/whatsmyuseragent.js
@@ -1,13 +1,5 @@
-const { firefox } = require("playwright");
+const { example } = require("../src/helpers");
 
-(async () => {
-  const browser = await firefox.launch();
-  const page = await browser.newPage();
-
-  console.log("Visiting page");
-  await page.goto('http://whatsmyuseragent.org/');
-  await new Promise(resolve => setTimeout(resolve, 5000));
-
-  console.log("Saving recording");
-  await browser.close();
-})();
+example("whatsmyuseragent", async (page) => {
+  await page.goto("http://whatsmyuseragent.org/");
+});
