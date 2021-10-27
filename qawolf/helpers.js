84d7819c28ea34dc929120125b54f7d8067e5aba
Jason Laster <jason.laster.11@gmail.com>
2021-10-26 19:52:03 -0700

    tweaks on top of qawolf tests

--- qawolf/helpers.js
+++ qawolf/helpers.js
@@ -1,11 +1,12 @@
 const { assertElement, assertText } = require("qawolf");
 const faker = require("faker");
+require("dotenv").config();
 
-async function launch(
-  { browser: browserName, headless } = { headless: false }
-) {
+async function launch({ headless } = { headless: false }) {
   const playwright = require("@recordreplay/playwright");
-  const browser = await playwright[browserName || "chromium"].launch({
+  let browserName = process.env.PLAYWRIGHT_CHROMIUM ? "chromium" : "firefox";
+
+  const browser = await playwright[browserName].launch({
     headless,
   });
   const context = await browser.newContext();
