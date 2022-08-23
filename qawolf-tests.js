f3e2c728d206c89f96042001573d908cbd4cee67
Ryan Duffy <ryan@replay.io>
2022-08-23 09:55:18 -0700

    add metrics

--- qawolf-tests.js
+++ qawolf-tests.js
@@ -1,4 +1,5 @@
 const { readdirSync } = require("fs");
+const fetch = require("node-fetch");
 
 function getTests() {
   const exclude = ["getInbox.js", "helpers.js", "framer_click_layer.js"];
@@ -13,31 +14,68 @@ async function recordTest(test) {
     stdio: "inherit",
   });
 
-  return [test, proc.status];
+  return proc.status;
+}
+
+async function pingTestMetrics(
+  recordingId, // : string | undefined,
+  test // : {
+  // id: string;
+  // duration: number;
+  // recorded: boolean;
+  // }
+) {
+  const runId = process.env.TEST_RUN_ID;
+  const webhookUrl = process.env.RECORD_REPLAY_WEBHOOK_URL;
+
+  if (!webhookUrl) {
+    console.log(
+      "RECORD_REPLAY_WEBHOOK_URL is undefined. Skipping test metrics"
+    );
+    return;
+  }
+
+  if (!runId) {
+    console.log("TEST_RUN_ID is undefined. Skipping test metrics");
+    return;
+  }
+
+  try {
+    return await fetch(`${webhookUrl}/api/metrics`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        type: "test.finished",
+        recordingId,
+        test: {
+          ...test,
+          runId,
+        },
+      }),
+    });
+  } catch (e) {
+    console.log("Failed to send test metrics", e);
+  }
 }
 
 (async () => {
   const tests = getTests();
 
-  let running = 0;
-  const int = setInterval(() => {
-    if (tests.length === 0) {
-      console.log("All tests completed!");
-      clearInterval(int);
-      return;
+  while (tests.length) {
+    const test = tests.shift();
+    console.log(`Starting ${test}`);
+    const startTime = Date.now();
+    try {
+      const status = await recordTest(test);
+      console.log(`Test ${test} ${status ? "failed" : "passed"}`);
+    } catch (e) {
+      console.error(`Recording crashed`, e);
+    } finally {
+      await pingTestMetrics(undefined, {
+        id: test + (process.env.PLAYWRIGHT_CHROMIUM ? "-chromium" : "-firefox"),
+        duration: Date.now() - startTime,
+        recorded: !process.env.RECORD_REPLAY_NO_RECORD,
+      });
     }
-    if (running < 5) {
-      const test = tests.shift();
-      console.log(`Starting ${test}`);
-      running++;
-      recordTest(test)
-        .then(async ([test, status]) => {
-          console.log(`Test ${test} ${status ? "failed" : "passed"}`);
-        })
-        .catch((e) => console.error(`Recording crashed`, e))
-        .finally(() => {
-          running--;
-        });
-    }
-  }, 100);
+  }
 })();
